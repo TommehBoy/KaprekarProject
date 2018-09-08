f7589a2f0b01d630701bdb0387dd8f613602f31d
Thomas Casburn <42653575+TommehBoy@users.noreply.github.com>
2018-09-07 20:06:02 -0500

    Add files via upload
    Moved int n declaration line above first if statement

--- kaprekarDemo.c
+++ kaprekarDemo.c
@@ -5,12 +5,13 @@
 
 int main(int argc, char **argv) {
 
+  int n = atoi(argv[1]);
   if(argc != 2) {
     printf(stderr, "Usage: %s n\n", argv[0]);
     exit(1);
   }
 
-  int n = atoi(argv[1]);
+ 
 
   if(n !=0) {
     printf("%d is a Kaprekar Number!\n", n);
